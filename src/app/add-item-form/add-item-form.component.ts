93d0ee60034f47621814e821a725c03a7e5ccc96
AndrewJBateman <gomezbateman@yahoo.com>
2021-05-19 14:03:55 +0200

    updated to Angular v12

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -9,9 +9,8 @@ import { BudgetItem } from "src/shared/models/budget-item.model";
 })
 export class AddItemFormComponent implements OnInit {
   @Input() item: BudgetItem;
-  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<
-    BudgetItem
-  >();
+  @Output() formSubmit: EventEmitter<BudgetItem> =
+    new EventEmitter<BudgetItem>();
 
   isNewItem: boolean;
 
